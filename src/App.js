71277f0382a14ae8312dd8239632b1fcec2fab72
guille-m369 <guille.ma369@gmail.com>
2023-07-05 12:16:55 -0300

    nuevo tmano

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         <button onClick={() => handleButtonClick(2160, 3840)}>Open Camera 3840x2160</button>
         <button onClick={() => handleButtonClick(2160, 1080)}>Open Camera 2160x1080</button>
         <button onClick={() => handleButtonClick(720, 720)}>Open Camera 720x720</button>
+        <button onClick={() => handleButtonClick(1080, 2160)}>Open Camer 1080x2160</button>
       </div>
       <style>{videoClass}</style>
       <Webcam ref={webcamRef} className="video" />
