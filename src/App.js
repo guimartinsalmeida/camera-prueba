2e4786b59cf1d23f11383f50374e3dfd96278516
Guilherme Martins de Almeida <guimartinsalmeida@MacBook-Pro-de-Guilherme.local>
2023-11-03 17:08:35 -0300

    new camera size

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <button onClick={() => handleButtonClick(2160, 1080)}>Open Camera 2160x1080</button>
         <button onClick={() => handleButtonClick(720, 720)}>Open Camera 720x720</button>
         <button onClick={() => handleButtonClick(1080, 2160)}>Open Camera 1080x2160</button>
-        <button onClick={() => handleButtonClick(1920, 1080)}>Open Camera 1920 x 1080</button>
+        <button onClick={() => handleButtonClick(1080, 1920)}>Open Camera 1920 x 1080</button>
 
       </div>
       <style>{videoClass}</style>
